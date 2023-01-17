a5891417f2f8bfe21d2a05cf75ed15bbf0525a58
blacksg <sagwa.nth@gmail.com>
2023-01-17 12:31:36 +0900

    [Add] Summer/Winter Coding(~2018) - 소수 만들기

---
+++ src/main/kotlin/programmers/summer_winter_coding/소수_만들기/Solution.kt
@@ -0,0 +1,31 @@
+package programmers.summer_winter_coding.소수_만들기
+
+class Solution {
+    fun solution(nums: IntArray): Int {
+        fun prime(num: Int): Boolean {
+            if (num == 1) {
+                return false
+            }
+            var i = 2
+            while (i * i <= num) {
+                if (num % i++ == 0) {
+                    return false
+                }
+            }
+            return true
+        }
+        
+        var answer = 0
+        val lastIndex = nums.lastIndex
+        for (i in 0..(lastIndex - 2)) {
+            for (j in (i + 1)..(lastIndex - 1)) {
+                for (k in (j + 1)..lastIndex) {
+                    if (prime(nums[i] + nums[j] + nums[k])) {
+                        answer++
+                    }
+                }
+            }
+        }
+        return answer
+    }
+}
