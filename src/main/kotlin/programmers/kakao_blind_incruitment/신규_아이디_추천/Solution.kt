cb4b83e9a5c427f2f6c0598113aa3cd8ca58d0cc
blacksg <sagwa.nth@gmail.com>
2023-02-12 21:14:37 +0900

    [Add] 2021 KAKAO BLIND RECRUITMENT - 신규 아이디 추천

---
+++ src/main/kotlin/programmers/kakao_blind_incruitment/신규_아이디_추천/Solution.kt
@@ -0,0 +1,50 @@
+package programmers.kakao_blind_incruitment.신규_아이디_추천
+
+class Solution {
+    fun solution(new_id: String): String {
+        return new_id.lowercase()
+            .removeInvalidCharacters()
+            .removeDuplicatedPeriods()
+            .removeSurroundingPeriods()
+            .fillIfEmpty()
+            .trimAndRemoveSuffixPeriod()
+            .extend()
+    }
+    
+    private fun String.removeInvalidCharacters(): String {
+        return this.replace(Regex("[^a-z\\d-_.]"), "")
+    }
+    
+    private fun String.removeDuplicatedPeriods(): String {
+        return this.replace(Regex("\\.{2,}"), ".")
+    }
+    
+    private fun String.removeSurroundingPeriods(): String {
+        return this.removePrefix(".").removeSuffix(".")
+    }
+    
+    private fun String.fillIfEmpty(): String {
+        if (this.isEmpty()) {
+            return "a"
+        }
+        return this
+    }
+    
+    private fun String.trimAndRemoveSuffixPeriod(): String {
+        if (this.length > 15) {
+            return this.substring(0, 15).removeSuffix(".")
+        }
+        return this
+    }
+    
+    private fun String.extend(): String {
+        if (this.length < 3) {
+            val builder = StringBuilder(this)
+            while (builder.length < 3) {
+                builder.append(this.last())
+            }
+            return builder.toString()
+        }
+        return this
+    }
+}
